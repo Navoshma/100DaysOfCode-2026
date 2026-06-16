0a4bb8f44a3a4691110e4b4d1bd2c9b0aa7f9ad4
Navoshma Mittal <mittalnavoshma@gmail.com>
2026-06-16 22:10:34 +0530

    Create Q1_leetcode.java

---
+++ DSA/Navoshma_590012676/Day_2/Q1_leetcode.java
@@ -0,0 +1,26 @@
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; 
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; 
+                k++; 
+            }
+        }
+        
+        return k; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 2, 3};
+        int val1 = 3;
+        int result1 = removeElement(nums1, val1);
+        System.out.println("Remaining count: " + result1); 
+
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int result2 = removeElement(nums2, val2);
+        System.out.println("Remaining count: " + result2); 
+    }
+}
