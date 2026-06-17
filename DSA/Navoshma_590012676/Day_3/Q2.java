43d995c29cd82e0ab79b1576360aa2027dd43803
Navoshma Mittal <mittalnavoshma@gmail.com>
2026-06-17 22:06:15 +0530

    Create Q2.java

---
+++ DSA/Navoshma_590012676/Day_3/Q2.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Main {
+
+    public static int firstOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int lastOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int first = firstOccurrence(arr, target);
+
+        if (first == -1) {
+            System.out.println(0);
+            return;
+        }
+
+        int last = lastOccurrence(arr, target);
+
+        System.out.println(last - first + 1);
+    }
+}
