f747590a32646bae3323e4001e108fe6be4673fc
Navoshma Mittal <mittalnavoshma@gmail.com>
2026-06-19 22:00:33 +0530

    Create Q2.java

---
+++ DSA/Navoshma_590012676/Day_5/Q2.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean checkEqualArrays(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : a) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : b) {
+            if (!map.containsKey(num)) {
+                return false;
+            }
+
+            map.put(num, map.get(num) - 1);
+
+            if (map.get(num) == 0) {
+                map.remove(num);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
